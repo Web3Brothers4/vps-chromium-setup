080abf39fd4a8e4dd4ab43b7d2ef7ec3be1e300e
Web3Brothers <web3brothers.official@gmail.com>
2024-11-30 18:25:42 +0530

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -5,10 +5,6 @@ GREEN="\033[0;32m"     # Green
 YELLOW="\033[1;33m"    # Bright Yellow
 NC="\033[0m"           # No Color
 
-# Update VPS
-echo -e "${YELLOW}Updating VPS...${NC}"
-sudo apt-get update && sudo apt-get upgrade -y
-
 # Install prerequisites
 echo -e "${YELLOW}Installing Dependencies...${NC}"
 sudo apt install -y curl ca-certificates
@@ -77,15 +73,3 @@ echo -e "${YELLOW}Starting Chromium container...${NC}"
 docker-compose up -d
 
 echo -e "${YELLOW}Chromium container setup completed! You can access it via the configured ports (3050:3000, 3051:3001), e.g., http://<ipaddress>:3050.${NC}"
-
-# Thank you message
-echo "==================================="
-echo -e "${YELLOW}           CryptonodeHindi       ${NC}"
-echo "==================================="
-echo -e "${GREEN}    Thanks for using this script!${NC}"
-echo "==================================="
-echo -e "${YELLOW}Twitter: @CryptonodeHindi${NC}"
-echo -e "${YELLOW}YouTube: https://www.youtube.com/@CryptonodesHindi${NC}"
-echo -e "${YELLOW}Medium: https://medium.com/@cryptonodehindi${NC}"
-echo -e "${YELLOW}Join our Telegram for any support: https://t.me/cryptonodehindi${NC}"
-echo "======================================================================"
